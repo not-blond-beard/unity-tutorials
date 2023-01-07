dfe304fad33f08cade40b7b956fccae5f9d2a680
upa-r-upa <duekthtjf783@gmail.com>
2023-01-08 02:32:15 +0900

    Tutorial 4 complete

--- PandemoniumPlatformerGame-uparupa/Assets/Scripts/PlayerMovement.cs
+++ PandemoniumPlatformerGame-uparupa/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,26 +14,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
-
     }
 
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
+        //Flip player when moving left-right
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-
-        // Set animator parameters
+        //Set animator parameters
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
 
+        //Wall jump logic
         if (wallJumpCooldown > 0.2f)
         {
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
@@ -58,17 +58,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isGrounded())
         {
-
             body.velocity = new Vector2(body.velocity.x, jumpPower);
-
             anim.SetTrigger("jump");
         }
         else if (onWall() && !isGrounded())
         {
             if (horizontalInput == 0)
             {
-
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
                 transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
             else
@@ -78,20 +75,19 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collsion)
-    {
-    }
 
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
-
     private bool onWall()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(
-            boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
