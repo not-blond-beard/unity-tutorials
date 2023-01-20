85cc00745f88c7189053d76ee1b2a9daaabe7c25
upa-r-upa <duekthtjf783@gmail.com>
2023-01-21 04:07:52 +0900

    Tutorial 7 complete

---
+++ PandemoniumPlatformerGame-uparupa/Assets/Scripts/Health/Healthbar.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
