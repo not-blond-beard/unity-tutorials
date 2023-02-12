0a3eb41f0dbeb79a035a5d338ffb15f3c3dd1e08
upa-r-upa <duekthtjf783@gmail.com>
2023-02-13 01:55:14 +0900

    9강 - 1/3

---
+++ PandemoniumPlatformerGame-uparupa/Assets/Scripts/Enemies/EnemyDamage.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
